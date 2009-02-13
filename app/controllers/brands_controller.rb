abd001e2b4f846942b7996ab21bb0bb8a2e24c98
Kevin Edwards <kev@Macintosh-2.local>
2009-02-13 16:04:03 +0000

    create graphiing stuff

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,6 +2,10 @@ class BrandsController < ApplicationController
   
   def show
     @brand = Brand.find(params[:id])
+    
+    @graph_data = Brandwatch.graph_data(@brand)
+    
+    raise @graph_data.inspect
   end
   
   
